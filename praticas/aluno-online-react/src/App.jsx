6e9d134c2aa789b2a068794a39b1c6e02f2fcc46
manumanullll <manuela.dewes@iesb.edu.br>
2026-05-16 18:18:38 -0300

    fix: resolve conflitos de caminhos mapeando a nova estrutura de pastas

--- praticas/aluno-online-react/src/App.jsx
+++ praticas/aluno-online-react/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import MainLayout from './components/layout/MainLayout/index';
+import Layout from './components/Layout/Layout';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Faltas from './pages/Faltas/Faltas';
 import Notas from './pages/Notas/Notas';
@@ -10,14 +10,13 @@ import Login from './pages/Login/Login';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route path="/login" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Layout />}>
         <Route index element={<Dashboard />} />
         <Route path="faltas" element={<Faltas />} />
         <Route path="notas" element={<Notas />} />
         <Route path="boletos" element={<Boletos />} />
         <Route path="requerimentos" element={<Requerimentos />} />
-
       </Route>
     </Routes>
   );
